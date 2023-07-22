dbdedea403217c6d8b9bf95b51752e2cb161b7f5
MarcosVSBlanco <marcos.blanco@hotmail.com>
2023-07-22 11:06:05 -0300

    finalizando aula dados

---
+++ CursoProgUdemy/Fundamentos/Inicio/desafio_area.js
@@ -0,0 +1,8 @@
+// PI = 3.141592
+// PI * raio * raio
+
+let raio = 9;
+const PI = 3.141592;
+let circunferencia = (PI * raio * raio);
+
+console.log('A circunferencia é de ' + circunferencia + 'm2' );
---
+++ CursoProgUdemy/Fundamentos/Inicio/desafio_troca.js
@@ -0,0 +1,12 @@
+let a = 7;
+let b = 94;
+let memoria
+
+//depois da troca... a = 94 e b = 7
+
+memoria = a;
+a = b;
+b = memoria;
+
+console.log(a);
+console.log(b);
